6af7eec78ce05b78fe2c2ee06469d4f1e50ff464
yanyanyan <yancfm@gmail.com>
2021-08-13 13:52:36 -0300

    mudanca no .sh + maquina do yan

--- src/test/run.sh
+++ src/test/run.sh
@@ -1,3 +1,6 @@
+echo "[WARNING] papi needs to get permission to read system events"
+sudo sh -c 'echo 0 >/proc/sys/kernel/perf_event_paranoid'
+
 for counter in {2048..128000..128}; 
 do 
 ./test.bin $counter mergesort;
